6a1203d6b9f4418b368f1e71f9cb70afcf8d4320
PolinaTimofeeva <polleyw@mail.ru>
2022-02-23 21:10:12 +0300

    final changes

--- js/main.js
+++ js/main.js
@@ -4,8 +4,8 @@ $(document).ready(function() {
     // Modal window 1
 
     $('#modal__btn-1').on('click', function() {
-        $('.modal__body-1').hide('fast', function() {
-            $('.thanks__modal-1').show('fast');
+        $('.modal__body-1').fadeOut('fast', function() {
+            $('.thanks__modal-1').fadeIn('fast');
         })
     });
 
@@ -13,39 +13,63 @@ $(document).ready(function() {
     // Modal window 2
 
     $('#modal__open-2').on('click', function() {
-            $('.modal-2').show('fast');
+            $('.modal-2').fadeIn('fast');
             $('.modal-2').css({display:'flex'});
     });
 
     $('#modal__btn-2').on('click', function() {
-        $('.modal__body-2').hide('fast', function() {
-            $('.thanks__modal-2').show('fast');
+        $('.modal__body-2').fadeOut('fast', function() {
+            $('.thanks__modal-2').fadeIn('fast');
         })
     });
 
-    $('.modal__close').on('click', function(){
-        $('.modal-2').hide('fast');
-    });
+    $(".modal-2, .modal__close").on("click", function (e) {
+        if (e.target == this) {
+            $(".modal-2").fadeOut('fast');
+        };
+      });
+
 
 
     // Modal window 3
 
     $('#modal__open-3').on('click', function() {
-        $('.modal-3').show('fast');
+        $('.modal-3').fadeIn('fast');
         $('.modal-3').css({display:'flex'});
     });
 
     $('#modal__btn-3').on('click', function() {
-        $('.modal__body-3').hide('fast', function() {
-            $('.thanks__modal-3').show('fast');
+        $('.modal__body-3').fadeOut('fast', function() {
+            $('.thanks__modal-3').fadeIn('fast');
         })
     });
 
-    $('.modal__close').on('click', function(){
-        $('.modal-3').hide('fast');
+
+    $(".modal-3, .modal__close").on("click", function (e) {
+        if (e.target == this) {
+            $(".modal-3").fadeOut(300);
+        };
+    });
+
+    // Modal window 4 
+
+    $('#modal__open-4').on('click', function() {
+        $('.modal-4').fadeIn('fast');
+        $('.modal-4').css({display:'flex'});
+    });
+
+    $('#modal__btn-4').on('click', function() {
+        $('.modal__body-4').fadeOut('fast', function() {
+            $('.thanks__modal-4').fadeIn('fast');
+        })
     });
 
 
+    $(".modal-4, .modal__close").on("click", function (e) {
+        if (e.target == this) {
+            $(".modal-4").fadeOut(300);
+        };
+    });
 
     // Phone mask
 
